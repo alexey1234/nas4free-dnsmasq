c124d10346b930fc1eca471c44e1ca38488e4096
alexey1234 <alexeykr68@gmail.com>
2013-12-23 22:07:27 +0200

    Update change_ver.php

--- sbin/change_ver.php
+++ sbin/change_ver.php
@@ -5,36 +5,29 @@ $dnsmasqversion=0;
 $workdir_1 = file("/tmp/dnsmasqinstaller");
 $workdir = trim($workdir_1[0]);
 $mainfile = file("{$workdir}/temporary/www/extensions_dnsmasq_server.php");
-$version_1 = preg_split ( "/VERSION_NBR, 'v/", $mainfile[1]);
-//here
+$version_1 = preg_split ( "/Version/", $mainfile[4]);
 $currentversion=substr($version_1[1],0,3);
-if (is_array($config['thebrig'])) {
-		if ($config['thebrig']['rootfolder']) { 
-			$thebrigrootfolder = $config['thebrig']['rootfolder'];
-			$thebrigversion = $config['thebrig']['version'];
-			if ($thebrigversion == $currentversion) {
+if (is_array($config['dnsmasq'])) {
+		if ($config['dnsmasq']['rootfolder']) { 
+			$dnsmasqrootfolder = $config['dnsmasq']['rootfolder'];
+			$installed_file = file($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_server.php");
+			$version_2 = preg_split ( "/Version/", $mainfile[4]);
+			$installed_version=substr($version_2[1],0,3);
+			if ($installed_version == $currentversion) {
 				$message = "No need updates \n"; 
 				if (is_file("/tmp/thebrigversion") ) unlink ("/tmp/thebrigversion");
 				goto met1;
 				}
-			elseif ( $thebrigversion == 1 )  {
-				$message = "You use first thebrig version \n";
-				$config['thebrig']['version'] = $currentversion;
-				write_config();
-				file_put_contents("/tmp/thebrigversion", "updated");
-				}
 			else {
-				$message = "You use old thebrig version, we reinstall it \n";
-				$config['thebrig']['version'] = $currentversion;
-				write_config();
-				file_put_contents("/tmp/thebrigversion", "updated");
+				$message = "You use old dnsmasq version, we update it to current\n";
+				file_put_contents("/tmp/dnsmasqversion", "updated");
 				}
 			}
-		else { $message = "You cannot have Thebrig installed"; 
-		file_put_contents("/tmp/thebrigversion", "installed");
+		else { $message = "You cannot have Dnsmasq installed"; 
+		file_put_contents("/tmp/dnsmasqversion", "installed");
+			}
 		}
-		}
-	else { $message = "Hello new user, We will install TheBrig now \n";
-	file_put_contents("/tmp/thebrigversion", "installed"); }
+	else { $message = "Hello new user, We will install Dnsmasq now \n";
+	file_put_contents("/tmp/dnsmasqversion", "installed"); }
 met1 : echo $message;
 ?>
