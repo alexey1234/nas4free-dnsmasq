ee70b8931f578a77e6d20a2b868b34d6a698c1fc
Alexey <alexeykr68@gmail.com>
2013-12-24 16:58:08 +0200

    repair faultly inputs

--- sbin/start_dnsmasq.php
+++ sbin/start_dnsmasq.php
@@ -13,12 +13,12 @@ if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_server.php") ) c
 if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_hosts.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_hosts.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_hosts.php");else {}
 if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_conf.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_conf.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_conf.php");else {}
 if ( ! is_file ( "/usr/local/www/ext/dnsmasq/menu.inc") ) copy ($config['dnsmasq']['rootfolder']."www/menu.inc","/usr/local/www/ext/dnsmasq/menu.inc");else {}
-if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_log.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_conf.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_log.php");else {}
-if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_clients.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_conf.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_clients.php");else {}
-if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_check.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_conf.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_check.php");else {}
-if ( ! is_file ( "/usr/local/www/ext/dnsmasq/warn.png") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_conf.php","/usr/local/www/ext/dnsmasq/warn.png");else {}
-if ( ! is_file ( "/usr/local/www/ext/dnsmasq/gradient_listhdr1_good.png") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_conf.php","/usr/local/www/ext/dnsmasq/gradient_listhdr1_good.png");else {}
-if ( ! is_file ( "/usr/local/www/ext/dnsmasq/gradient_listhdr1_bad.png") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_conf.php","/usr/local/www/ext/dnsmasq/gradient_listhdr1_bad.png");else {}
+if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_log.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_log.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_log.php");else {}
+if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_clients.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_clients.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_clients.php");else {}
+if ( ! is_file ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_check.php") ) copy ($config['dnsmasq']['rootfolder']."www/extensions_dnsmasq_check.php","/usr/local/www/ext/dnsmasq/extensions_dnsmasq_check.php");else {}
+if ( ! is_file ( "/usr/local/www/ext/dnsmasq/warn.png") ) copy ($config['dnsmasq']['rootfolder']."www/warn.png","/usr/local/www/ext/dnsmasq/warn.png");else {}
+if ( ! is_file ( "/usr/local/www/ext/dnsmasq/gradient_listhdr1_good.png") ) copy ($config['dnsmasq']['rootfolder']."www/gradient_listhdr1_good.png","/usr/local/www/ext/dnsmasq/gradient_listhdr1_good.png");else {}
+if ( ! is_file ( "/usr/local/www/ext/dnsmasq/gradient_listhdr1_bad.png") ) copy ($config['dnsmasq']['rootfolder']."www/gradient_listhdr1_bad.png","/usr/local/www/ext/dnsmasq/gradient_listhdr1_bad.png");else {}
 // create links to webroot
 if (!is_link( "/usr/local/www/extensions_dnsmasq_clients.php") ) symlink ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_clients.php","/usr/local/www/extensions_dnsmasq_clients.php");else {}
 if (!is_link( "/usr/local/www/extensions_dnsmasq_log.php") ) symlink ( "/usr/local/www/ext/dnsmasq/extensions_dnsmasq_log.php","/usr/local/www/extensions_dnsmasq_log.php");else {}
