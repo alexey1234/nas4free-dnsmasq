302649d1d8ed5039e35236aed3b4f79553b7d225
alexey1234 <alexeykr68@gmail.com>
2015-08-08 18:32:47 +0300

    Update extensions_dnsmasq_conf.php

--- www/extensions_dnsmasq_conf.php
+++ www/extensions_dnsmasq_conf.php
@@ -8,15 +8,15 @@ require("guiconfig.inc");
 if (!isset($config['dnsmasq']) || !is_array($config['dnsmasq'])) $config['dnsmasq']=array();
 if (!isset($config['dnsmasq']['rootfolder']) ) {
 		if(file_exists( '/tmp/dnsmasq.tmp' )  ) {
-			$config['dnsmasq']['rootfolder'] = rtrim( file_get_contents('/tmp/dnsmasq.tmp') );
+			$pconfig['rootfolder'] = rtrim( file_get_contents('/tmp/dnsmasq.tmp') );
+			if ( $pconfig['rootfolder'][strlen($pconfig['rootfolder'])-1] != "/")  { 
+				$pconfig['rootfolder'] = $pconfig['rootfolder'] . "/";	
+				}
+		}	else { 		
+		$input_errors[] = "Install procedure filed, please reinstall"; 	
+		} 
+	} else { $pconfig['rootfolder'] = $config['dnsmasq']['rootfolder'] ;}
 	
-			if ( $config['dnsmasq']['rootfolder'][strlen($config['dnsmasq']['rootfolder'])-1] != "/")  { $config['dnsmasq']['rootfolder'] = $config['dnsmasq']['rootfolder'] . "/";	}
-			$pconfig['rootfolder'] = $config['dnsmasq']['rootfolder'];
-	
-			}
-		else { 		$input_errors[] = "Dnsmasq not installed"; 	} 
-		}
-	$pconfig['rootfolder'] = $config['dnsmasq']['rootfolder'];	
 if ($_POST) {
 	
 	unset($input_errors);
@@ -74,13 +74,11 @@ if ($_POST) {
 $pgtitle = array(gettext("Extensions"),gettext("DNSMASQ"), gettext("config"));
 include("fbegin.inc"); ?>
 <script language="JavaScript">
-function disable_buttons() {
-	document.iform.Submit.disabled = true;
-	document.iform.submit();}
+
 function message(obj) {
-	if (obj.checked) {
+	
 		alert('If you want to uninstall the DNSMASQ, please make sure that all files have been removed');
-	}
+
 		return true;
 }
 </script>
@@ -104,26 +102,59 @@ else if ($savemsg) { print_info_box($savemsg); }
 		<form action="extensions_dnsmasq_conf.php" method="post" name="iform" id="iform">
 		<table width="100%" border="0" cellpadding="6" cellspacing="0">
 		<?php html_titleline(gettext("Extension dnsmasq basic setting"));?>
-		<?php html_inputbox("rootfolder", gettext("Extension working folder"), $pconfig['rootfolder'], gettext("folder where extension sets"), true, 50);?>
-		
-		<?php html_separator();?>		
-		<?php html_titleline(gettext(" remove extension"));?>
-		
-		<!-- This is the row beneath the title -->
-		<tr><td width="22%" valign="top" class="vncellreq">&nbsp;</td>
+		<?php html_inputbox("rootfolder", "Extension working folder", $pconfig['rootfolder'], "folder where extension sets", true, 50);
+		html_separator();
+		if (!isset($config['dnsmasq']['rootfolder']) ) { ?>
+		<tr><td width="22%" valign="top" class="vncell">&nbsp;Action</td>
 			<td width="78%" class="vtable">
-				<input type="checkbox" name="remove" value="1" onclick="return message(this);"> check  for remove extension <b>Need attension</B></input>
+			 	<input name="Submit" type="submit" class="formbtn" value="Confirm">
 			</td>
 		</tr>
+		<?php } else { ?>
+			<tr><td width="22%" valign="top" class="vncell">&nbsp;Action</td>
+			<td width="78%" class="vtable">
+				<input name="Submit" type="submit" class="formbtn" value="Remove" onclick="return message(this);">
+			</td>
+		</tr>
+		<?php html_separator();?>
+		</table>
+	<?php include("formend.inc");	?>
+</form>	
+		<form action="extensions_dnsmasq_conf.php" method="post" name="iform" id="iform">
+		<table width="100%" border="0" cellpadding="6" cellspacing="0">
+		
+		
+		<?php 
+		$connected = @fsockopen("www.github.com", 80); 
+		if ( $connected ) {
+			fclose($connected);
+			$ctx = stream_context_create(['ssl' => ["verify_peer"=>false,"verify_peer_name"=>false,]]);
+			//$arrContextOptions=array( "ssl"=>array( "verify_peer"=>false, "verify_peer_name"=>false ),);  
+			$gitserverfile = file_get_contents("https://raw.githubusercontent.com/alexey1234/nas4free-dnsmasq/master/dnsmasq_install.sh", false, $ctx);
+			file_put_contents("/tmp/dnsmasq_install.sh", $gitserverfile);
+			if (1==preg_match("/^#Ver.+/m", $gitserverfile,$matched)) {
+				$gitversion = preg_split ( "/\s/", $matched[0]);
+			} else {
+				$gitversion[] = "ERROR connect to github";
+			}
+		$localfile = file_get_contents($config['dnsmasq']['rootfolder']."dnsmasq_install.sh");
+		if (1==preg_match("/^#Ver.+/m", $localfile,$matched1)) {
+				$localversion = preg_split ( "/\s/", $matched1[0]);
+			} else {
+				$localversion[] = "ERROR find local installator";
+			}
 			
-		<!-- This is the Save button -->
-		<tr><td width="22%" valign="top">&nbsp;</td>
-			<td width="78%">
-			 	<input name="Submit" type="submit" class="formbtn" value="Action" onClick="disable_buttons();">
+			?> 
+	
+			<tr><td width="22%" valign="top" class="vncell">&nbsp;Current status</td>
+			<td width="78%" class="vtable" >The latest version on GitHub is: <?=$gitversion[1];?><br />Your version is: <?=$localversion[1];?><br />
+	
+			 	<input name="Submit" type="submit" class="formbtn" value="Update">
 			</td>
 		</tr>
+		<?php } } ?>
 	</table>
-	<?php include("formend.inc");?>
+	<?php include("formend.inc");	?>
 </form>
 
 
