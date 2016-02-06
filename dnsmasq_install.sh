57e8788b303206d20770ae7d247d5028d931ba93
alexey1234 <alexeykr68@gmail.com>
2016-02-06 20:08:08 +0200

    Change install. Obtain bin from FreeBSD

--- dnsmasq_install.sh
+++ dnsmasq_install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#Version 0.2
+#Version 0.3
 
 . /etc/rc.subr
 . /etc/util.subr
@@ -7,24 +7,31 @@
 
 exerr () { echo -e "$*" >&2 ; exit 1; }
 #
-START_FOLDER=`pwd`
+STAT=$(procstat -f $$ | grep -E "/"$(basename $0)"$")
+FULL_PATH=$(echo $STAT | sed -r s/'^([^\/]+)\/'/'\/'/1 2>/dev/null)
+START_FOLDER=$(dirname $FULL_PATH | sed 's|/dnsmasq_install.sh||')
+
 
 # Store the script's current location in a file
 echo $START_FOLDER > /tmp/dnsmasqinstaller
-# Check, if use fresh install or upgrade 
-_rootfolder=`configxml_get "//dnsmasq/rootfolder"`
-if [ ! -z $_rootfolder ]; then
-	DMAS_ROOT=$_rootfolder
-	/etc/rc.d/dnsmasq stop
-	rm -f /usr/local/sbin/dnsmasq
-	rm -f /etc/rc.d/dnsmasq
-else
-# use first argument as path to root folder
-	DMAS_ROOT=$1
-	if [ ! -d $DMAS_ROOT ]; then
+
+#Store user's inputs
+# This first checks to see that the user has supplied an argument
+if [ ! -z $1 ]; then
+    # The first argument will be the path that the user wants to be the root folder.
+    # If this directory does not exist, it is created
+    DMAS_ROOT=$1    
+    
+    # This checks if the supplied argument is a directory. If it is not
+    # then we will try to create it
+    if [ ! -d $DMAS_ROOT ]; then
         echo "Attempting to create a new destination directory....."
         mkdir -p $DMAS_ROOT || exerr "ERROR: Could not create directory!"
     fi
+else
+# We are here because the user did not specify an alternate location. Thus, we should use the 
+# current directory as the root.
+    DMAS_ROOT=$START_FOLDER
 fi
 # create working folder	
 mkdir -p $START_FOLDER/temporary || exerr "ERROR: Could not create install directory!"
@@ -36,37 +43,53 @@ cd $START_FOLDER/temporary || exerr "ERROR: Could not access install directory!"
 echo "Retrieving the most recent version of dnsmasq"
 fetch https://github.com/alexey1234/nas4free-dnsmasq/archive/master.zip || exerr "ERROR: Could not write to install directory!"
 
-
 # Extract the files we want, stripping the leading directory, and exclude
 # the git nonsense
 echo "Unpacking the tarball..."
 tar -xvf master.zip --exclude='.git*' --strip-components 1
 # Get rid of the tarball
 rm master.zip
-echo "dnsmasq install/update"
-if [ `uname -p` = "amd64" ]; then
-	echo "Renaming 64 bit dnsmasq binary"
-	mv sbin/dnsmasq_64 sbin/dnsmasq
-	rm sbin/dnsmasq_86
+
+# Obtain binary from FreeBSD package
+echo "Fetch package from FreeBSD..."
+pkg fetch -y -o tmp dnsmasq
+cd tmp/All
+tar -xzf dnsmasq*.txz
+cd ../..
+rm -f sbin/dmsmasq_64
+rm -f sbin/dmsmasq_86
+cp tmp/All/usr/local/sbin/dnsmasq sbin/dmsmasq
+rm -fr tmp
+
+
+# Check, if use fresh install or upgrade 
+_rootfolder=`configxml_get "//dnsmasq/rootfolder"`
+if [ ! -z $_rootfolder ]; then
+# Update current config
+	DMAS_ROOT=$_rootfolder
+	/etc/rc.d/dnsmasq stop
+	rm -f /usr/local/sbin/dnsmasq
+	rm -f /etc/rc.d/dnsmasq
+	cp -f -R $START_FOLDER/temporary/* $DMAS_ROOT/
 else
-	echo "Renaming 32 bit dnsmasq binary"
-	mv sbin/dnsmasq_86 sbin/dnsmasq
-	rm sbin/dnsmasq_64
-fi
-cp -r * $DMAS_ROOT/
-mkdir -p /usr/local/www/ext
-ln -s $DMAS_ROOT/dnsmasq /usr/local/www/ext/dnsmasq
-cd /usr/local/www
+	echo "Look like fresh install"
+	cp -f -R $START_FOLDER/temporary/* $DMAS_ROOT/
+#create webroot files
+	mkdir -p /usr/local/www/ext
+	ln -s $DMAS_ROOT/dnsmasq /usr/local/www/ext/dnsmasq
+	cd /usr/local/www
 # For each of the php files in the extensions folder
-for file in /usr/local/www/ext/dnsmasq/*.php
-do
-# Check if the link is already there
-	if [ -e "${file##*/}" ]; then
-		rm "${file##*/}"
-	fi
+	for file in /usr/local/www/ext/dnsmasq/*.php
+	do
+	# Check if the link is already there
+		if [ -e "${file##*/}" ]; then
+			rm "${file##*/}"
+		fi
 		# Create link
-	ln -s "$file" "${file##*/}"
-done
+		ln -s "$file" "${file##*/}"
+	done
+fi
+chmod 755 $DMAS_ROOT/sbin/*
 echo "Congratulations! dnsmasq was updated/installed . Navigate to config web page and push Save "
 
 # Clean after work
